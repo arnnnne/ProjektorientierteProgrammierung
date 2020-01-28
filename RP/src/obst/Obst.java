19228b7f6a3f639e758eda481bf80fa1eb65b740
arnnnne <Lenovo@10.105.60.173>
2020-01-28 08:58:53 +0100

    New

---
+++ RP/src/obst/Obst.java
@@ -0,0 +1,10 @@
+package obst;
+
+public class Obst {
+	
+	String name;
+	double gewicht;
+	double vitamingehalt;
+	boolean reife;
+
+}
---
+++ RP/src/obst/Programm.java
@@ -0,0 +1,10 @@
+package obst;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
