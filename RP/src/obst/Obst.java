3f4f4408b7f8e28f56d501a447743ec594b5d315
arnnnne <Lenovo@10.105.60.173>
2020-02-05 10:26:56 +0100

    ää

--- RP/src/obst/Obst.java
+++ RP/src/obst/Obst.java
@@ -1,10 +1,66 @@
 package obst;
 
 public class Obst {
-	
-	String name;
-	double gewicht;
-	double vitamingehalt;
-	boolean reife;
 
+	private String name;
+	private double gewicht;
+	private double vitamingehalt;
+	private boolean reife;
+
+	/**
+	 * Konstruktor.
+	 * Wird nur aufgerufen, wenn ein Objekt erzeugt wird.
+	 * @param Der Name des Obstes
+	 */
+	public Obst(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Gibt den Namen des Objektes zurück.
+	 * @return Der Name des Obstes.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/** 
+	 * Gibt das Gewicht des Obstes zurück
+	 * @return Das Gewicht
+	 */
+	public double getGewicht() {
+		return gewicht;
+	}
+
+	/**
+	 * Ändert das Gewicht des Obstes.
+	 * @param gewicht Das neue Gewicht des Obstes
+	 */
+	public void setGewicht(double gewicht) {
+		this.gewicht = gewicht;
+	}
+	
+	public double getVitamingehalt() {
+		return vitamingehalt;
+	}
+
+	/**
+	 * Ändert den Vitamingehalt des Obstes.
+	 * @param vitamingehalt Der neue Vitamingehalt des Obstes
+	 */
+	public void setVitamingehalt(double vitamingehalt) {
+		this.vitamingehalt = vitamingehalt;
+	}
+	
+	public boolean isReife() {
+		return reife;
+	}
+
+	/**
+	 * Ändert die Reife des Obstes.
+	 * @param reife Die neue Reife des Obstes
+	 */
+	public void setReife(boolean reife) {
+		this.reife = reife;
+	}
 }
--- RP/src/obst/Programm.java
+++ RP/src/obst/Programm.java
@@ -3,8 +3,36 @@ package obst;
 public class Programm {
 
 	public static void main(String[] args) {
+		Obst banane = new Obst("Banane");
+		banane.setGewicht(100);
+		banane.setReife(true);
+		banane.setVitamingehalt(100);
 		
-
-	}
+		System.out.println(banane.getName());
+		System.out.println(banane.getGewicht());
+		System.out.println(banane.isReife());
+		System.out.println(banane.getVitamingehalt() + "\n");
+		
+		Obst apfel = new Obst("Apfel");
+		apfel.setGewicht(130);
+		apfel.setReife(true);
+		apfel.setVitamingehalt(100);
+		
+		System.out.println(apfel.getName());
+		System.out.println(apfel.getGewicht());
+		System.out.println(banane.isReife());
+		System.out.println(banane.getVitamingehalt() + "\n");
+		
+		Banane bananeNeu = new Banane();
+		bananeNeu.setKruemmung(10.5);
+		System.out.println(bananeNeu.getName());
+		System.out.println(bananeNeu.getKruemmung());
+		
+		Orange orangeNeu = new Orange();
+		orangeNeu.setKerne(true);
+		System.out.println("\n" +orangeNeu.getName());
+		System.out.println(orangeNeu.isKerne());
+		}
+	
 
 }
